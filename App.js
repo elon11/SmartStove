eef1cd6fb448ce7a3613c34706f21b66aba754f9
neriya zudi <neriyazudi@gmail.com>
2022-12-06 23:20:42 +0200

    starting design home page

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import LOGO from "./assets/logo_project.png";
+
+const LOGO_SRC = Image.resolveAssetSource(LOGO).uri;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> test test</Text>
+      <Text> Smart Stove </Text>
+      <Image source={{ uri: LOGO_SRC }} style={{ width: 400, height: 400 }} />
       <StatusBar style='auto' />
     </View>
   );
